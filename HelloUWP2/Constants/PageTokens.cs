6d5d7dcfc07ff1c41cf55f0456ec8028de4b9b92
Gihun Ham <ham@e-mirim.hs.kr>
2021-04-07 08:47:45 +0900

    초기 커밋.

---
+++ HelloUWP2/Constants/PageTokens.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloUWP2
+{
+    internal static class PageTokens
+    {
+        public const string MainPage = "Main";
+    }
+}
---
+++ HelloUWP2/Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using HelloUWP2.ViewModels;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace HelloUWP2.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var myDlg = new MessageDialog("안녕하");
+            myDlg.Commands.Add(new UICommand("OK"));
+            myDlg.Commands.Add(new UICommand("안OK"));
+            myDlg.Commands.Add(new UICommand("싫음"));
+            await myDlg.ShowAsync();
+        }
+    }
+}
